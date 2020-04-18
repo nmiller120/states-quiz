63a382c468416071f8f7527c1f5ccf5c1741224f
nmiller120 <45175452+nmiller120@users.noreply.github.com>
2020-04-18 11:11:34 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -66,7 +66,7 @@ function onSubmit() {
 	if (document.getElementById("returnLink") == null) {
 		var returnLink = document.createElement("a");
 		returnLink.id = "returnLink";
-		returnLink.href = "http:\\\\192.168.1.114\\HTML%20Projects\\home.htm";
+		returnLink.href = "\\index.html";
 		returnLink.innerHTML = "Return to Home";
 		document.body.appendChild(returnLink);
 	}
